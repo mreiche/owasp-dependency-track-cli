68db2d859d9ca8fc0b145db383518875a427cbcd
Mike Reiche <mike@reiche.world>
2025-05-21 20:58:10 +0200

    Fix sending project version and retrieving correct project on test. Add upload command.

--- owasp_dt_cli/test.py
+++ owasp_dt_cli/test.py
@@ -1,48 +1,21 @@
 from datetime import datetime
 from math import floor
-from pathlib import Path
 from time import sleep
 
 from is_empty import empty
-from owasp_dt.api.bom import upload_bom
 from owasp_dt.api.event import is_token_being_processed_1
 from owasp_dt.api.project import get_projects
-from owasp_dt.models import UploadBomBody, IsTokenBeingProcessedResponse, BomUploadResponse
+from owasp_dt.models import IsTokenBeingProcessedResponse, Project
+from tinystream import Stream
 
 from owasp_dt_cli import api, config
 from owasp_dt_cli.log import LOGGER
 from owasp_dt_cli.output import print_findings_table
+from owasp_dt_cli.upload import handle_upload
 
 
 def handle_test(args):
-    sbom_file: Path = args.sbom
-    assert sbom_file.exists(), f"{sbom_file} doesn't exists"
-
-    assert not empty(args.project_uuid) or not empty(args.project_name), "At least a project UUID or a project name is required"
-
-    client = api.create_client_from_env()
-    body = UploadBomBody(
-        is_latest=args.latest,
-        auto_create=args.auto_create,
-        bom=sbom_file.read_text()
-    )
-    if args.project_uuid:
-        body.project = args.project_uuid
-
-    if args.project_name:
-        body.project_name = args.project_name
-
-    if args.parent_uuid:
-        body.parent_uuid = args.parent_uuid
-
-    if args.parent_name:
-        body.parent_name = args.parent_name
-
-    resp = upload_bom.sync_detailed(client=client, body=body)
-    assert resp.status_code != 404, "Project not found"
-
-    upload = resp.parsed
-    assert isinstance(upload, BomUploadResponse), upload
+    upload, client = handle_upload(args)
 
     wait_time = 2
     test_timeout_sec = int(config.getenv("TEST_TIMEOUT_SEC", "300"))
@@ -63,8 +36,25 @@ def handle_test(args):
     if empty(args.project_uuid):
         resp = get_projects.sync_detailed(client=client, name=args.project_name, page_size=10)
         projects = resp.parsed
-        assert len(projects) == 1, f"Multiple projects found matching '{args.project_name}'"
-        args.project_uuid = projects[0].uuid
+        stream = Stream(projects)
+        if args.project_version:
+            def _filter_version(project: Project):
+                return project.version == args.project_version
+            stream = stream.filter(_filter_version)
+
+        if args.latest:
+            def _filter_latest(project: Project):
+                return project.is_latest == args.latest
+            stream = stream.filter(_filter_latest)
+
+        def _compare_last_bom_import(a: Project, b: Project):
+            return b.last_bom_import - a.last_bom_import
+
+        opt = stream.sort(_compare_last_bom_import).next()
+        assert opt.present, "Previous scanned project not found"
+
+        project = opt.get()
+        args.project_uuid = project.uuid
 
     findings = api.get_findings_by_project_uuid(client=client, uuid=args.project_uuid)
     if len(findings):
--- test/test_test.py
+++ test/test_test.py
@@ -4,7 +4,9 @@ import httpx
 import pytest
 
 from common import load_env
+from owasp_dt_cli.api import create_client_from_env
 from owasp_dt_cli.args import create_parser
+from owasp_dt.api.project import get_projects
 
 __base_dir = Path(__file__).parent
 
@@ -19,10 +21,24 @@ def test_test():
         "test-project",
         "--auto-create",
         "--latest",
+        "--project-version",
+        "maus",
         str(__base_dir / "test.sbom.xml"),
     ])
 
+    assert args.latest == True
+    assert args.project_version == "maus"
+
     args.func(args)
+
+@pytest.mark.depends(on=['test_test'])
+#@pytest.mark.xfail(reason="Version doesn't get updated by OWASP DT")
+def test_uploaded():
+    client = create_client_from_env()
+    resp = get_projects.sync_detailed(client=client, name="test-project")
+    projects = resp.parsed
+    assert projects[0].version == "maus"
+    assert projects[0].is_latest == True
 
 def test_proxy_fails(monkeypatch):
     monkeypatch.setenv("HTTP_PROXY", "http://localhost:3128")
