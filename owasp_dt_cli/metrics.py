d66b9c95d9e4f4225ce23b4676b24b67ed3df81c
Mike Reiche <mike@reiche.world>
2025-10-23 19:20:04 +0200

    Add support for passing group name to metrics (#29)

--- owasp_dt_cli/metrics.py
+++ owasp_dt_cli/metrics.py
@@ -4,7 +4,8 @@ import owasp_dt
 import prometheus_client as prometheus
 from owasp_dt.api.finding import get_all_findings_1
 from owasp_dt.api.violation import get_violations
-from owasp_dt.models import PolicyViolation, Finding
+from owasp_dt.api.project import create_project, get_projects, patch_project, delete_projects, get_project
+from owasp_dt.models import PolicyViolation, Finding, Project
 
 from owasp_dt_cli import api
 from owasp_dt_cli.common import schedule
@@ -17,8 +18,8 @@ def handle_prometheus_metrics(args):
     registry = prometheus.REGISTRY
     adapter.disable_python_metrics(registry)
 
-    cvss_score = prometheus.Gauge(adapter.prefix_metric_key("cvss_score"), "Project CVEs and their scoring", ["project_name", "component_name", "cve", "cvss_version", "severity"], registry=registry)
-    violations = prometheus.Gauge(adapter.prefix_metric_key("policy_violations"), "Project Policy violations", ["project_name", "component_name", "policy_name", "state"], registry=registry)
+    cvss_score = prometheus.Gauge(adapter.prefix_metric_key("cvss_score"), "Project CVEs and their scoring", ["project_name", "group_name", "component_name", "cve", "cvss_version", "severity"], registry=registry)
+    violations = prometheus.Gauge(adapter.prefix_metric_key("policy_violations"), "Project Policy violations", ["project_name", "group_name", "component_name", "policy_name", "state"], registry=registry)
     client = api.create_client_from_env()
     active_project_names = []
 
@@ -36,7 +37,6 @@ def handle_prometheus_metrics(args):
 
         active_project_names = current_project_names.keys()
 
-
     if args.serve:
         prometheus.start_http_server(args.serve_port, addr="0.0.0.0")
         LOGGER.info(f"Started server at http://localhost:{args.serve_port}")
@@ -53,39 +53,39 @@ def update_finding_metrics(
         instrument: prometheus.Gauge,
 ) -> dict[str, bool]:
     current_active_projects: dict[str, bool] = {}
+    project_cache: dict[str, Project] = {}
 
-    def _add_findings(findings:list[Finding]):
+    def _add_findings(findings: list[Finding]):
         for finding in findings:
             vulnerability = finding.vulnerability
             component = finding.component
             current_active_projects[component.project_name] = True
 
+            project_uuid = str(component.project)
+            if project_uuid not in project_cache:
+                resp = get_project.sync_detailed(client=client, uuid=project_uuid)
+                assert resp.status_code in [200]
+                project_cache[project_uuid] = resp.parsed
 
+            project = project_cache[project_uuid]
+
+            def _add_score(version: str, cvss: float):
+                instrument.labels(*[
+                    component.project_name,
+                    project.group,
+                    component.name,
+                    vulnerability.vuln_id,
+                    version,
+                    vulnerability.severity,
+                ]).set(cvss)
 
             if vulnerability.cvss_v3_base_score and vulnerability.cvss_v3_base_score > 0:
-                instrument.labels(*[
-                    component.project_name,
-                    component.name,
-                    vulnerability.vuln_id,
-                    "v3",
-                    vulnerability.severity,
-                ]).set(vulnerability.cvss_v3_base_score)
+                _add_score("v3", vulnerability.cvss_v3_base_score)
             elif vulnerability.cvss_v2_base_score and vulnerability.cvss_v2_base_score > 0:
-                instrument.labels(*[
-                    component.project_name,
-                    component.name,
-                    vulnerability.vuln_id,
-                    "v2",
-                    vulnerability.severity,
-                ]).set(vulnerability.cvss_v2_base_score)
+                _add_score("v2", vulnerability.cvss_v2_base_score)
             else:
-                instrument.labels(*[
-                    component.project_name,
-                    component.name,
-                    vulnerability.vuln_id,
-                    "v3",
-                    vulnerability.severity,
-                ]).set(0.1)
+                _add_score("v3", 0.1)
+
     try:
         resp = get_all_findings_1.sync_detailed(
             client=client,
@@ -100,11 +100,13 @@ def update_finding_metrics(
 
     return current_active_projects
 
+
 def update_violation_metrics(
         client: owasp_dt.Client,
         instrument: prometheus.Gauge,
 ) -> dict[str, bool]:
     current_active_projects: dict[str, bool] = {}
+
     def _add_violations(violations: list[PolicyViolation]):
         for violation in violations:
             component = violation.component
@@ -125,6 +127,7 @@ def update_violation_metrics(
                 page_number=page_number,
                 page_size=1000,
             )
+
         for violations in api.page_result(_loader):
             _add_violations(violations)
     except Exception as e:
