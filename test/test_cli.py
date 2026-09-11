99dac135d9935557273738f093da8e1b016d7492
Mike Reiche <mike@reiche.world>
2026-09-11 14:44:49 +0200

    fix: Improve error handling (#42)
    * fix: Improve error handling
    
    * feat: Add test for ValueErrors

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,3 +1,5 @@
+import sys
+
 import pytest
 
 from owasp_dt_cli import cli
@@ -8,3 +10,16 @@ def test_cli():
         cli.main()
 
     assert e.value.code == 1
+
+def test_cli_with_invalid_env_fails():
+    sys.argv = [
+        "owasp-dt-cli",
+        "--env",
+        "katze",
+        "metrics",
+        "prometheus",
+    ]
+    with pytest.raises(expected_exception=SystemExit) as e:
+        cli.main()
+
+    assert e.value.code == 2
